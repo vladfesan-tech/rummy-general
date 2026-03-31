08dc67e18d125593af0b82647c2db095c51a267f
vladfesan-tech <vlad.fesan@gmail.com>
2026-04-01 00:00:35 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'v1_cache_scor';
+// Adaugă aici TOATE fișierele pe care vrei să le salvezi offline
+const ASSETS_TO_CACHE = [
+  './',
+  'index.html',
+  'manifest.json'
+];
+
+// Instalarea: Salvează fișierele în memoria telefonului
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Am salvat fișierele în cache!');
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activarea: Șterge cache-ul vechi dacă faci update
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+});
+
+// Interceptarea cererilor: Dacă nu e net, ia-le din cache
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
